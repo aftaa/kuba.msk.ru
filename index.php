d6ea35b74db9694725e535f69314242675e997d8
Maxim Gabidullin <after@ya.ru>
2020-03-12 19:31:57 +0300

    bootstrap + jquery etc.

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+?><!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>k . u . b . a</title>
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap-grid.css">
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap-reboot.css">
+    <script src="/js/jquery-3.4.1.min.js"></script>
+    <script src="/js/popper.min.js"></script>
+    <script src="/js/bootstrap.js"></script>
+    <script src="/js/bootstrap.bundle.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="/css/kuba.msk.css">
+</head>
+<body>
+<main><h1>Куба</h1>
+    <a href="https://yandex.ru/maps/213/moscow/?ll=37.639955%2C55.616943&z=15"
+       target="_blank"><span style="color: red;">Я</span>andex.maps</a> /
+    <a href="https://n.maps.yandex.ru/#!/?z=15&ll=37.639955%2C55.616943&l=nk%23sat" target="_blank">Народная
+        карта</a> /
+    <a href="https://www.google.ru/maps/@55.6169425,37.6399289,14z">Google maps</a> /
+    <a href="http://wikimapia.org/#lang=ru&lat=55.617358&lon=37.646842&z=16&m=w">Wikimapia</a></main>
+
+</body>
+</html>
