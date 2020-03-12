aee0aec14774df4687f66497aa43ca65e3df9a63
Maxim Gabidullin <after@ya.ru>
2020-03-12 23:15:07 +0300

    favicons

--- index.php
+++ index.php
@@ -16,46 +16,48 @@
 <body>
 <main>
     <h1>Куба / <small>14-й &laquo;В&raquo; квартал Ленино-Дачного</small></h1>
-    <div id="maps">на картах:
+    <div id="maps">
         <a href="https://yandex.ru/maps/213/moscow/?ll=37.639955%2C55.616943&z=15"
-           target="_blank">Yandex.maps</a> /
+           target="_blank">Yandex.maps</a><img alt="Куба на Яндекс картах" src="/pics/favicons/yandex.maps.svg" width="16" height="16"> /
         <a href="https://n.maps.yandex.ru/#!/?z=15&ll=37.639955%2C55.616943&l=nk%23sat" target="_blank">Народная
-            карта</a> /
-        <a href="https://www.google.ru/maps/@55.6169425,37.6399289,14z">Google maps</a> /
-        <a href="http://wikimapia.org/#lang=ru&lat=55.617358&lon=37.646842&z=16&m=w">Wikimapia</a>
+            карта</a><img alt="Куба на народной карте" src="/pics/favicons/n.maps.yandex.ru.ico" width="16" height="16"> /
+        <a href="https://www.google.ru/maps/@55.6169425,37.6399289,14z">Google maps</a><img alt="Куба на гугл картах" src="/pics/favicons/google.maps.jpg" style="margin-left: 5px" width="16" height="16"> /
+        <a href="http://wikimapia.org/#lang=ru&lat=55.617358&lon=37.646842&z=16&m=w">Wikimapia</a><img alt="Куба на Викимапии" src="/pics/favicons/wikimaia.ico" style="margin-left: 3px" width="16" height="16">
     </div>
-    <hr size="1">
+
+
+
 
     <div id="text" class="container">
         <div class="row">
-            <p class="col-lg-6">Старожил района, активный участник инициативной группы Аршиновского парка Любовь
+            <h4 class="font-italic" style="margin-bottom: 2rem;">Старожил района, активный участник инициативной группы Аршиновского парка Любовь
                 Евгеньевна Кузнецова
                 по&nbsp;нашей
                 просьбе любезно подготовила очерк на&nbsp;очень часто возникающий вопрос (особенно у&nbsp;молодого
                 населения):
-                почему &laquo;Кубу&raquo; называют &laquo;Кубой&raquo;.</p>
+                почему &laquo;Кубу&raquo; называют &laquo;Кубой&raquo;.</h4>
 
-            <p class="col-lg-6 yellow">В&nbsp;нашем районе &quot; Царицыно &quot; есть небольшой микрорайон&nbsp;&mdash;
+            <p>В&nbsp;нашем районе &quot; Царицыно &quot; есть небольшой микрорайон&nbsp;&mdash;
                 14В квартал Ленино
                 -Дачное.
                 Местные жители его называют
                 &laquo;КУБА&raquo;.</p>
 
-            <p class="col-lg-6">Основной район Ленино -Дачное был построен в&nbsp;советское время 1965-1969&nbsp;г.
+            <p>Основной район Ленино -Дачное был построен в&nbsp;советское время 1965-1969&nbsp;г.
                 Последние дома
                 ограничивались
                 ул.Севанской д.17,19,21. Не&nbsp;было школы N868, детских садов, Зиловской больницы (сейчас больница&nbsp;им.
                 В.М.
                 Буянова).</p>
 
-            <p class="col-lg-6 yellow">14В мкр. Ленино-Дачное&nbsp;&mdash; это островок застройки из&nbsp;десятка домов,
+            <p>14В мкр. Ленино-Дачное&nbsp;&mdash; это островок застройки из&nbsp;десятка домов,
                 стоящих на&nbsp;холмах.
                 Островок со&nbsp;всех сторон был окружен водой. Имеет форму треугольника: две стороны пересекающие
                 пути
                 железных
                 дорог Курской&nbsp;и&nbsp;Павелецкой, третья река Котляковка, впадающая в&nbsp;реку Городню.</p>
 
-            <p class="col-lg-6">Рядом
+            <p>Рядом
                 <a href="https://msk-tsaritsino.livejournal.com/tag/%D0%90%D1%80%D1%88%D0%B8%D0%BD%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9%20%D0%BF%D0%B0%D1%80%D0%BA"
                    target="_blank">Аршиновский парк</a>, около него 2&nbsp;пруда, питающиеся частично водами реки
                 Котляковки
@@ -66,22 +68,22 @@
                 между
                 заболоченными прудами дамбу (пешеходная дорога) и началось строительство нового микрорайона 14В.</p>
 
-            <p class="col-lg-6 yellow">Появился новый маршрут автобуса N&nbsp;217&nbsp;с конечной остановкой &laquo;14&nbsp;квартал
+            <p>Появился новый маршрут автобуса N&nbsp;217&nbsp;с конечной остановкой &laquo;14&nbsp;квартал
                 Ленино-Дачное
                 &raquo;.</p>
 
-            <p class="col-lg-6">Но&nbsp;еще долгое время эта конечная остановка была на&nbsp;Севанской улице (у&nbsp;нынешнего
+            <p>Но&nbsp;еще долгое время эта конечная остановка была на&nbsp;Севанской улице (у&nbsp;нынешнего
                 Сбербанка).
                 Вот
                 местные жители и назвали эту стройку на&nbsp;острове КУБА. Название закрепилось до&nbsp;сих пор.</p>
 
-            <p class="col-lg-6 yellow">Связь с&nbsp;большой землей только через один маршрут автобуса и&nbsp;сейчас. Дорога
+            <p>Связь с&nbsp;большой землей только через один маршрут автобуса и&nbsp;сейчас. Дорога
                 в&nbsp;школу
                 N868,
                 больницу
                 и&nbsp;другой цивилизации через овраг, пруды&nbsp;&mdash; долго и&nbsp;далеко.</p>
 
-            <p class="col-lg-6">Но&nbsp;есть одно большое преимущество для жителей Кубы: необыкновенная природа, чистый
+            <p>Но&nbsp;есть одно большое преимущество для жителей Кубы: необыкновенная природа, чистый
                 воздух, по&nbsp;весне
                 пение соловьев, прогулки в&nbsp;Аршиновском парке, загорание на&nbsp;прудах, рыбная ловля, зимой
                 катание
@@ -98,8 +100,12 @@
         </div>
 
         <hr size="1">
-        <p>&copy;&nbsp;<a href="https://msk-tsaritsino.livejournal.com/542603.html" target="_blank">Наша
-                &laquo;Куба&raquo;</a></p>
+        <p class="font-italic">
+            Источник:&nbsp;
+            <a href="https://msk-tsaritsino.livejournal.com/542603.html" target="_blank">
+                Наша &laquo;Куба&raquo;
+            </a>
+        </p>
         <br>
     </div>
 </main>
